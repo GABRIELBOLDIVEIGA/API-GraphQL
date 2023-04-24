dbd239a097e76115147d66c4d48e89e2378419c2
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-23 21:58:06 -0300

    ...

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -4,6 +4,10 @@ class UsersAPI extends RESTDataSource {
   constructor(){
     super()
     this.baseURL = 'http://localhost:3000'
+    this.respostaCustom = {
+      code: 200,
+      mensagem: "Operação realizada com sucesso!"
+    }
   }
 
   async getUsers() { 
@@ -38,21 +42,24 @@ class UsersAPI extends RESTDataSource {
   }
 
   async atualizaUser (novosDados) {
-    const role = await this.get(`/roles?type=${novosDados.role}`)
+    const role = await this.get(`/roles?type=${novosDados.user.role}`)
     await this.put(`users/${novosDados.id}`, {
-      ...novosDados,
+      ...novosDados.user,
       role: role[0].id
     })
 
     return({
-      ...novosDados,
-      role: role[0]
+      ...this.respostaCustom,
+      user: {
+        ...novosDados.user,
+        role: role[0]
+      },
     })
   }
 
   async deletaUser (id) {
     await this.delete(`/users/${id}`)
-    return `Usuario de ID: ${id} deletado...`
+    return this.respostaCustom
   }
 }
 
