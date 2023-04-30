4c49d25629d4a2273f9748dff5391053889e551e
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-29 22:28:55 -0300

    ...

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server')
 const { mergeTypeDefs } = require('graphql-tools')
 const path = require('path')
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const { userSchema, userResolvers, UsersAPI } = require('./user')
 const { turmaSchema, turmaResolvers, TurmasAPI } = require('./turma')
@@ -11,7 +11,7 @@ const { matriculaSchema, matriculaResolvers, MatriculasAPI } = require('./matric
 const typeDefs = mergeTypeDefs([userSchema, turmaSchema, matriculaSchema])
 const resolvers = [userResolvers, turmaResolvers, matriculaResolvers]
 
-const MONGODB = "";
+// const MONGODB = "mongodb+srv://hugobt:<@H200711300h>@cluster0.wxegcih.mongodb.net/test";
 
 const dbConfig = {
   client: 'sqlite3',
@@ -33,15 +33,15 @@ const server = new ApolloServer( {
   },
  })
 
- mongoose.connect(MONGODB, {useNewUrlParser: true})
-  .then(() => {
-    console.log("Mongo conectado");
-    return server.listen({port: process.env.PORT || 4000});
-  }).then((res) => {
-    console.log("Server rodando: ", res)
-  })
+//  mongoose.connect(MONGODB)
+//   .then(() => {
+//     console.log("Mongo conectado");
+//     return server.listen({port: process.env.PORT || 4000});
+//   }).then((res) => {
+//     console.log("Server rodando: ", res)
+//   })
 
-// server.listen().then(({url}) => {
-//   console.log(`Servidor rodando na porta ${url}`)
-// })
-server.listen({port: process.env.PORT || 4000})
+server.listen().then(({url}) => {
+  console.log(`Servidor rodando na porta ${url}`)
+})
+// server.listen({port: process.env.PORT || 4000})
